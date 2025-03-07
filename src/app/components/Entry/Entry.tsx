7905bde11ced4b87fdaad301dbe6caaa190d3046
Rudra-Dey-Sarkar <rudradeysarkar5@gmail.com>
2025-03-07 14:58:43 +0530

    fixed

--- src/app/components/Entry/Entry.tsx
+++ src/app/components/Entry/Entry.tsx
@@ -1,12 +1,15 @@
+"use client"
+import Link from 'next/link'
 import React from 'react'
 
 function Entry() {
     return (
         <div className='grid justify-center gap-y-5'>
             <p className='text-[2rem] text-center sm:text-[2.5rem] text-[#191919] font-[600]'>Carfted for your all academic needs</p>
-            <button className="bg-[#212121] text-white m-auto w-fit py-2 px-7 rounded-[5px] hover:cursor-pointer">
+            <Link href="/log-reg" 
+            className="bg-[#212121] text-white m-auto w-fit py-2 px-7 rounded-[5px] hover:cursor-pointer">
                 Get your Entry
-            </button>
+            </Link >
         </div>
     )
 }
--- src/app/components/Note/Note.tsx
+++ src/app/components/Note/Note.tsx
@@ -1,4 +1,5 @@
 import { ArrowRight } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 function Note() {
@@ -8,9 +9,11 @@ function Note() {
                 <p className='text-[2rem] sm:text-[2.5rem] font-[600]'>How we come to this?</p>
                 <p>We observed and researched for three years in a variety of student. Where The best needs more attention to do better and the student struggling needs more attention to improve their own abilities. All students need personal attention. Based on the notion of “Personal attention” we have built our algorithm and Ai tutor to cater each children uniquely…</p>
                 {/* Read the full story button */}
-                <button className="flex w-fit items-center border-b-1 border-white hover:cursor-pointer">
+                <Link
+                href="/log-reg"
+                className="flex w-fit items-center border-b-1 border-white hover:cursor-pointer">
                     Read the full story <ArrowRight size={19} />
-                </button>
+                </Link>
             </div>
         </div>
     )
