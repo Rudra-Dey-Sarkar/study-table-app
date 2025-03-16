93093988778d3bf15ce420025f4d11d20c61f072
Rudra-Dey-Sarkar <rudradeysarkar5@gmail.com>
2025-03-16 14:00:45 +0530

    fixed

--- src/app/components/ClientLayout/ClientLayout.tsx
+++ src/app/components/ClientLayout/ClientLayout.tsx
@@ -1,7 +1,6 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Toaster } from 'react-hot-toast';
-import { useRouter } from 'next/navigation';
 import { usePathname } from 'next/navigation';
 import Topbar from '../Topbar/Topbar';
 import Footer from '../Footer/Footer';
